3fb0b061495efa87fb132f711fa617a26cf4564f
Clement <clementthuet7@gmail.com>
2020-06-14 19:24:32 +0200

    Total rework

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,23 @@
 //caroussel
 var angle = 0;
-function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
+function galleryspin(sign, idSpinner) { 
+spinner = document.querySelector("#spinner"+idSpinner);
 if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
 spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+console.log(angle);
 }
 
-//navbar sticky
-$(document).ready(function() {
+//Change css property on scroll 
+/*$(document).ready(function() {
     var windowHeight = $(window).height();
     // Transition effect for navbar 
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > windowHeight*0.4) { 
+      if($(this).scrollTop() > windowHeight*0.4 && $( window ).width() > 991 ){ 
           $('.navbar').addClass('navbar-solid');
       } else {
           $('.navbar').removeClass('navbar-solid');
       }
     });
 });
+*/
