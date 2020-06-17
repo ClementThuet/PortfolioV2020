26272c4178da2e87752e45f5c5027a2e942a5e2a
Clement <clementthuet7@gmail.com>
2020-06-17 08:49:54 +0200

    Worked on carousel Added styles Added screenshots of projects

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,31 @@
 var angle = 0;
 function galleryspin(sign, idSpinner) { 
 spinner = document.querySelector("#spinner"+idSpinner);
-if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+if (sign) { angle = angle + 45; } else { angle = angle - 45; }
 spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-console.log(angle);
 }
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+$(document).ready(function() {
+    //if($(window).width() < 991 ){
+    if(isMobileDevice() ){ 
+        var myElement = document.getElementById('spinner3');
+        // create a simple instance
+        var mc = new Hammer(myElement);
+        console.log("this is a mobile device");
+        mc.on("panleft panright tap press", function(ev) {
+            ev.preventDefault();
+            if(ev.type === "panright" || ev.type === "press"){
+                galleryspin("-", 3);
+            }
+            else{
+                galleryspin("", 3);
+            }
+        });
+    }
+});
 
 //Change css property on scroll 
 /*$(document).ready(function() {
